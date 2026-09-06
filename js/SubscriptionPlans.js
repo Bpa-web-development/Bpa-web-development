00de01392fa7846396f9dad09a8a142b4bd38c41
HuckSolo <110217699+HuckSolo@users.noreply.github.com>
2026-09-06 17:18:04 -0400

    Subscription Plan Cards and potential navbar logo fix

--- js/SubscriptionPlans.js
+++ js/SubscriptionPlans.js
@@ -7,28 +7,37 @@ async function loadSubscriptionPlans() {
             throw new Error("Could not load SubscriptionPlanData.json");
         }
 
-        SubscriptionPlanData = await response.json();
-
+        const SubscriptionPlanData = await response.json();
+        buildSubscriptionPlans(SubscriptionPlanData);
     } 
     catch (error) {
         console.error("Subscription Plan Data error:", error);
     }
-    buildSubscriptionPlans(SubscriptionPlanData);
+    
 }
 
 function buildSubscriptionPlans(data) {
-    console.log(data)
     const container = document.getElementById("SubscriptionCardContainer");
-    container.innerHTML = data.map(plan => `
-        <div class="SubscriptionCard">
-            <h2>${plan.name}</h2>
-            <p>Price: ${plan.price}</p>
-            <p>${plan.description}</p>
-            <ul>
-                ${plan.features.map(feature => `<li>${feature}</li>`).join('')}
-            </ul>
-        </div>
-    `).join('');
+
+    let containerContent = '';
+
+    data.forEach(plan => {
+        console.log(plan)
+        containerContent += 
+            `   
+            <div class="SubscriptionCard">
+                <h1>${plan.name}</h1>
+                <p>Price: ${plan.price}</p>
+                <p>${plan.description}</p>
+                <img src="${plan.img}" alt="">
+                <ul class="SubscriptionFeatures">
+                    ${plan.features.map(feature => `<li>${feature}</li>`).join('')}
+                </ul>
+                <button>Subscribe</button>
+            </div>
+                `
+    })
+    container.innerHTML = containerContent;
 }
 
 
